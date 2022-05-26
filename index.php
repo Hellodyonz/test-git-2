53af69967a897a4b453ee1bbe11450a53554c592
Hellodyonz <hellodyonz@gmail.com>
2022-05-26 17:32:45 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello workd</h1>
     <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam reiciendis assumenda est cupiditate doloremque ducimus at quidem fuga, expedita eos, ab maiores recusandae, facere dolore esse sint architecto labore exercitationem.</h2>
+    <ul>
+        <li>new</li>
+        <li>one</li>
+    </ul>
 </body>
 </html>
